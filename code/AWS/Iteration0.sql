af3b68305ba5d5d2a68bc4c827a8d11fe36ad1c4
MillerDennis <miller.dennis4711@gmail.com>
2015-06-11 14:59:38 +0200

    Iteration0.sql updated *doku erweitert

--- code/AWS/Iteration0.sql
+++ code/AWS/Iteration0.sql
@@ -1,3 +1,3 @@
 INSERT OVERWRITE DIRECTORY 's3://de.whs.fdt.kaggle.acquire-valued-shoppers-challenge/output_data/iteration0/'
 SELECT DISTINCT (h.id), 0.0 AS repeatProbability
-FROM testhistory h;
+FROM test_history h;
